cb5233756a92e4c389f21ba118dd25c026dfeae8
Verminas <veremeeva96@gmail.com>
2024-04-29 15:41:02 +0300

    feat: add styles for section slogan

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import {SectionWrapper} from "../../../components/SectionWrapper";
 import {SectionTitle} from "../../../components/SectionTitle/SectionTitle";
 import {Link} from "../../../components/Link/Link";
+import {Container} from "../../../components/Container";
+import {FlexBoxWrapper} from "../../../components/FlexBoxWrapper";
+import {Button} from "../../../components/Button/Button";
 
 export const Slogan = () => {
   return (
     <SectionWrapper id={'slogan'}>
-      <SectionTitle>I Am Available For Freelance </SectionTitle>
-      <Link href={'#'}>hire me</Link>
+      <Container>
+        <FlexBoxWrapper direction={'column'} align={'center'} justify={'center'} gap={'0'}>
+          <SectionTitle>I Am Available For Freelance </SectionTitle>
+          <Button name={"hire me"}></Button>
+        </FlexBoxWrapper>
+      </Container>
     </SectionWrapper>
 );
 };
