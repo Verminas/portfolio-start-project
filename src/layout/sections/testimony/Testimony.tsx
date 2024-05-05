f3680ead89ca013a9e1fa0e5ea1be1ca79f8409e
Verminas <veremeeva96@gmail.com>
2024-05-05 23:42:42 +0300

    feat: add pixel perfect section testimony (mobile)

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -6,22 +6,34 @@ import {Slider} from "../../../components/Slider/Slider";
 import {Container} from "../../../components/Container";
 import {FlexBoxWrapper} from "../../../components/FlexBoxWrapper";
 import {WrapperIcon} from "../skills/Skill/Skill";
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
 
 export const Testimony = () => {
   return (
     <SectionWrapper id={'testimony'}>
       <Container>
-        <FlexBoxWrapper direction={'column'} align={'center'} gap={"35px"}>
+        <FlexBoxWrapperMedia direction={'column'} align={'center'} gap={"35px"}>
           <SectionTitle>Testimony</SectionTitle>
           <WrapperIcon>
             <Icon iconId={"quoteSvg"}/>
           </WrapperIcon>
           <Slider/>
-        </FlexBoxWrapper>
+        </FlexBoxWrapperMedia>
       </Container>
     </SectionWrapper>
   );
 };
+
+const FlexBoxWrapperMedia = styled(FlexBoxWrapper)`
+    @media ${theme.media.mobile} {
+      gap: 60px;
+      
+      p {
+        margin: 13px 0 25px;
+      }
+    }
+`
 
 
 
