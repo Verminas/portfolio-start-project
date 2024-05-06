9d857e1e4ae09d73bacbb9b516ac31f935af0bd4
Verminas <veremeeva96@gmail.com>
2024-05-06 13:50:44 +0300

    refactor: section main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,136 +1,24 @@
 import React from 'react';
 import {FlexBoxWrapper} from "../../../components/FlexBoxWrapper";
-import {SectionWrapper} from "../../../components/SectionWrapper";
 import imgProfile from "../../../assets/images/profile-photo.webp";
-import styled from "styled-components";
 import {Container} from "../../../components/Container";
-import {theme} from "../../../styles/Theme";
-import {font} from "../../../styles/Common";
+import {S} from "./Main_Styles";
 
-export const Main = () => {
+export const Main: React.FC = () => {
   return (
-    <SectionWrapperMedia id={'home'} height={'100vh'}>
+    <S.SectionWrapperMedia id={'home'} height={'100vh'}>
       <Container>
-        <FlexBoxWrapperMedia justify="space-around" align="center" gap={"0"} wrap={"wrap"}>
+        <S.FlexBoxWrapperMedia justify="space-around" align="center" gap={"0"} wrap={"wrap"}>
           <FlexBoxWrapper direction={'column'} gap={'0'} width={'fit-content'}>
-            <SmallText>Hi There</SmallText>
-            <Name>I am <span>Svetlana Dyablo</span></Name>
-            <Title>A Web Developer.</Title>
+            <S.SmallText>Hi There</S.SmallText>
+            <S.Name>I am <span>Svetlana Dyablo</span></S.Name>
+            <S.Title>A Web Developer.</S.Title>
           </FlexBoxWrapper>
-          <PhotoWrapper>
-            <Photo src={imgProfile} alt="profile photo"></Photo>
-          </PhotoWrapper>
-        </FlexBoxWrapperMedia>
+          <S.PhotoWrapper>
+            <S.Photo src={imgProfile} alt="profile photo"></S.Photo>
+          </S.PhotoWrapper>
+        </S.FlexBoxWrapperMedia>
       </Container>
-    </SectionWrapperMedia>
+    </S.SectionWrapperMedia>
 );
 };
-
-const FlexBoxWrapperMedia = styled(FlexBoxWrapper)`
-    @media ${theme.media.desktop} {
-        justify-content: space-between;
-    }
-    @media ${theme.media.tablet} {
-        align-content: center;
-    }
-    @media ${theme.media.mobile} {
-      align-content: flex-start;
-  }
-`
-const SectionWrapperMedia = styled(SectionWrapper)`
-    @media ${theme.media.tablet} {
-        height: fit-content;
-    }
-`
-
-const PhotoWrapper =styled.div`
-    position: relative;
-    object-fit: cover;
-    z-index: 1;
-
-    &::after {
-        content: "";
-        width: 360px;
-        height: 470px;
-        border: 5px solid ${theme.colors.accent};
-        position: absolute;
-        top: -25px;
-        left: 25px;
-        z-index: -1;
-        
-
-        @media ${theme.media.mobile} {
-            width: 314px;
-            height: 414px;
-            top: -20px;
-            left: 17px;
-        }
-    }
-
-    @media ${theme.media.tablet} {
-        margin-top: 65px;
-    }
-`
-
-const Photo = styled.img`
-    width: 350px;
-    min-width: 150px;
-    height: 430px;
-    object-fit: cover;
-    position: relative;
-    margin-right: 20px;
-
-    @media ${theme.media.desktop} {
-        margin-right: 0;
-    }
-    
-    @media ${theme.media.mobile} {
-        width: 310px;
-        height: 380px;
-    }
-    
-`
-
-const SmallText = styled.span`
-    font-size: 14px;
-    text-align: left;
-`
-
-const Name = styled.h2`
-    
-    ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmin: 36, Fmax: 50})}
-    letter-spacing: 0.05em;
-    text-align: left;
-    margin: 10px 0;
-    
-    span{
-        white-space: nowrap;
-        position: relative;
-        z-index: 1;
-        &::after{
-            content: "";
-            display: inline-block;
-            width: 100%;
-            height: 20px;
-            background-color: ${theme.colors.accent};
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            z-index: -1;
-        }
-    }
-
-    @media ${theme.media.mobile} {
-        margin: 15px 0 22px;
-    }
-`
-
-const Title = styled.h1`
-    font-size: 27px;
-    text-align: left;
-    ${font({Fmin: 20, Fmax: 27})}
-`
-
-
-
---
+++ src/layout/sections/main/Main_Styles.ts
@@ -0,0 +1,121 @@
+import styled from "styled-components";
+import {FlexBoxWrapper} from "../../../components/FlexBoxWrapper";
+import {theme} from "../../../styles/Theme";
+import {SectionWrapper} from "../../../components/SectionWrapper";
+import {font} from "../../../styles/Common";
+
+const SectionWrapperMedia = styled(SectionWrapper)`
+    @media ${theme.media.tablet} {
+        height: fit-content;
+    }
+`
+const FlexBoxWrapperMedia = styled(FlexBoxWrapper)`
+    @media ${theme.media.desktop} {
+        justify-content: space-between;
+    }
+    @media ${theme.media.tablet} {
+        align-content: center;
+    }
+    @media ${theme.media.mobile} {
+      align-content: flex-start;
+  }
+`
+
+const PhotoWrapper =styled.div`
+    position: relative;
+    object-fit: cover;
+    z-index: 1;
+
+    &::after {
+        content: "";
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.accent};
+        position: absolute;
+        top: -25px;
+        left: 25px;
+        z-index: -1;
+        
+
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+            top: -20px;
+            left: 17px;
+        }
+    }
+
+    @media ${theme.media.tablet} {
+        margin-top: 65px;
+    }
+`
+
+const Photo = styled.img`
+    width: 350px;
+    min-width: 150px;
+    height: 430px;
+    object-fit: cover;
+    position: relative;
+    margin-right: 20px;
+
+    @media ${theme.media.desktop} {
+        margin-right: 0;
+    }
+    
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
+    }
+    
+`
+
+const SmallText = styled.span`
+    font-size: 14px;
+    text-align: left;
+`
+
+const Name = styled.h2`
+    
+    ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmin: 36, Fmax: 50})}
+    letter-spacing: 0.05em;
+    text-align: left;
+    margin: 10px 0;
+    
+    span{
+        white-space: nowrap;
+        position: relative;
+        z-index: 1;
+        &::after{
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent};
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            z-index: -1;
+        }
+    }
+
+    @media ${theme.media.mobile} {
+        margin: 15px 0 22px;
+    }
+`
+
+const Title = styled.h1`
+    font-size: 27px;
+    text-align: left;
+    ${font({Fmin: 20, Fmax: 27})}
+`
+
+export const S = {
+  SectionWrapperMedia,
+  FlexBoxWrapperMedia,
+  PhotoWrapper,
+  Photo,
+  SmallText,
+  Name,
+  Title,
+}
